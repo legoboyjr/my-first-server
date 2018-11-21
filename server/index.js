8135ecc4474388b578a259b203c7e64f7e219a33
Alexander Thompson <legoboyjr@gmail.com>
2018-11-20 20:40:05 -0500

    set up pizza and cookies

--- server/index.js
+++ server/index.js
@@ -13,6 +13,31 @@ app.get('/hello/:name', (request, response) => {
         response.send(`Hello ${name}`)
 });
 
+app.get('/pizzas', (req,res) => {
+    const name = req.params.names;
+        res.status(200);
+        res.send({
+            pizzas: [
+                {
+                    name: 'pepperoni',
+                    price: 12.99
+                },
+                {
+                    name: "hawiian",
+                    price: 15.99
+                }
+            ]
+        })
+})
+
+app.get('/cookies', (req,res) => {
+    const name = req.params.names;
+        res.status(200);
+        res.send({
+            cookies: ['chocolate chip', 'penut butter', 'heath bar']
+        })
+})
+
 app.listen(port, () => {
     console.log(`now listening on port: ${port}`);
 }) 
